4a17d9d50f3ec7b67a6a361658f5a81b0c70df29
andres990420 <72096387+andres990420@users.noreply.github.com>
2025-01-22 08:06:46 -0400

    Update pokemon.js
    Se extrajo la funcion de obtenerHabilidadOculta a la carpeta de utils

--- src/entities/pokemon.js
+++ src/entities/pokemon.js
@@ -1,12 +1,14 @@
+import { obtenerHabilidadOculta } from "../utils/general.js";
+
 class Pokemon 
 {
   constructor(pokemon) {
     this.id = pokemon.id;
     this.name = pokemon.name;
     this.types = pokemon.types.map(type => type.type.name);
-    this.abilities = pokemon.abilities.map(ability => !ability.is_hidden ? ability.ability.name : null);
+    this.abilities = pokemon.abilities.map(ability => !ability.is_hidden ? ability.ability.name : undefined);
     this.sprite = pokemon.sprites.front_default;
-    this.hiddenAbility = pokemon.abilities.find(ability => ability.is_hidden).ability.name;
+    this.hiddenAbility = obtenerHabilidadOculta(pokemon.abilities);
   }
 };
 
