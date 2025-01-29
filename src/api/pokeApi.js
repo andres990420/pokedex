214b34f3caea3ee9df2f5f465526d27acb34cc9d
andres990420 <72096387+andres990420@users.noreply.github.com>
2025-01-28 20:38:04 -0400

    Update pokeApi.js
    Se agregaron llamados para la api de pokemon-species, types y ability. Tambien se cambio el URL_BASE por uno mas re-utilizable

--- src/api/pokeApi.js
+++ src/api/pokeApi.js
@@ -1,16 +1,42 @@
-const URL_BASE = 'https://pokeapi.co/api/v2/pokemon/';
+const URL_BASE = 'https://pokeapi.co/api/v2';
 
 
-export async function obtenerListaDePokemons(offset = 10000, limit = 0)
+export async function obtenerListaDePokemons(offset = 100000, limit = 0)
 {
-    const response = await fetch(`${URL_BASE}?offset=${limit}&limit=${offset}`);
+    const response = await fetch(`${URL_BASE}/pokemon?offset=${limit}&limit=${offset}`);
+    const listaPokemons =  await response.json();
+    return listaPokemons;
+}
+
+export async function obtenerEspeciesDePokemons(offset = 100000, limit = 0)
+{
+    const response = await fetch(`${URL_BASE}/pokemon-species?offset=${limit}&limit=${offset}`);
     const listaPokemons =  await response.json();
     return listaPokemons;
 }
 
 export async function obtenerPokemon(id)
 {
-    const response = await fetch(`${URL_BASE}/${id}`);
+    const response = await fetch(`${URL_BASE}/pokemon/${id}`);
     const pokemon = await response.json();
     return pokemon;
+}
+
+export async function obtenerEspeciePokemon(especie)
+{
+    const response = await fetch(`${URL_BASE}/pokemon-species/${especie}`);
+    const especiePokemon = await response.json();
+    return especiePokemon;
+}
+
+export async function obtenerHabilidadPokemon(habilidad) {
+    const response = await fetch(`${URL_BASE}/ability/${habilidad}`);
+    const habilidadPokemon = await response.json();
+    return habilidadPokemon;
+}
+
+export async function obtenerTipoPokemon(tipo) {
+    const response = await fetch(`${URL_BASE}/type/${tipo}`);
+    const tipoPokemon = await response.json();
+    return tipoPokemon;
 }
