954ad4c3f257b2a6f1b93002559e32b4a06b45eb
andres990420 <72096387+andres990420@users.noreply.github.com>
2025-01-28 20:45:03 -0400

    update general.js
    Se anadieron nuevas funciones al general.js como obtenerDescripcion, obtenerHabilidades, obtenerTipos y obtenerListaEspeciePokemons. Tambien se refactorizo el obtenerHabilidadOculta

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,8 +1,36 @@
+import { obtenerEspeciesDePokemons} from "../api/pokeApi.js";
+
 export function obtenerHabilidadOculta(habilidadesPokemon)
 {
   const habilidadOculta = habilidadesPokemon.find(ability => ability.is_hidden)
-  if(habilidadOculta)
-    {
-      return habilidadOculta.ability.name
-    }
+  return habilidadOculta ? habilidadOculta.ability.name : 'No posee habilidad oculta';
+}
+
+export function obtenerDescripcion(especiePokemon)
+{
+  const descripcion = especiePokemon.flavor_text_entries.find(data =>  data.language.name === 'es') ||  
+  especiePokemon.flavor_text_entries.find(data => data.language.name === 'en');
+  return descripcion ? descripcion.flavor_text : 'Descripcion no disponible';
+}
+
+export function obtenerHabilidades(habilidadesPokemon)
+{
+  const habilidades = []; 
+  habilidadesPokemon.map(ability => ability !== undefined ? habilidades.push(ability) : '');
+
+  return habilidades.length > 0 ? habilidades : 'Habilidades no disponible';
+}
+
+export function obtenerTipos(tiposPokemon)
+{
+  const tipos = [];
+  tiposPokemon.map(types => types.type.name != undefined ? tipos.push(types.type.name) : ''); 
+  return tipos.length > 0 ? tipos : 'Tipos no disponibles';
+}
+
+export async function obtenerListaEspeciePokemons() 
+{
+  const especiesPokemons = await obtenerEspeciesDePokemons();
+  const listaEspeciePokemons = especiesPokemons.results.map(pokemon => pokemon.name);
+  return listaEspeciePokemons;
 }
